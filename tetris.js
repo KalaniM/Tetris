6e9b7a37e01f7c1dbf62aa35abb1cbbaa4bd6636
KalaniM <kalakalanicontact@gmail.com>
2019-04-13 02:28:41 +0200

    let it move

--- tetris.js
+++ tetris.js
@@ -3,14 +3,21 @@ const context = canvas.getContext('2d'); //Le contexte à dessiner
 
 context.scale(20, 20); //Pour que les éléments soient plus gros, sinon ils sont tout pitits
 
-context.fillStyle = '#000'; //La couleur noire bg (oui c'est de toi que je parle)
-context.fillRect(0, 0, canvas.width, canvas.height); //width et height définies en html
 
 const matrix = [ //La première de nos pièces, en T
   [0, 0, 0],
   [1, 1, 1],
   [0, 1, 0], //Représentée par deux valeurs
-]; 
+];
+
+function draw() {
+
+  context.fillStyle = '#000'; //La couleur noire bg (oui c'est de toi que je parle)
+  context.fillRect(0, 0, canvas.width, canvas.height); //width et height définies en html
+  // Le fond est redraw a chaque update
+
+  drawMatrix(player.matrix, player.pos);
+}
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => { //Sélectionner chaques lignes
@@ -26,9 +33,28 @@ function drawMatrix(matrix, offset) {
   });
 }
 
+let dropCounter = 0;
+let dropInterval = 1000; 
+
+let lastTime = 0;
+function update(time = 0) { // L'animation se répète
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    player.pos.y++;
+    dropCounter = 0;
+  }
+
+  draw();
+  requestAnimationFrame(update);
+}
+
+
 const player = {
   pos : {x:5, y:5},
   matrix: matrix,
 }
 
-drawMatrix(matrix, {x:5, y: 5});
+update();
